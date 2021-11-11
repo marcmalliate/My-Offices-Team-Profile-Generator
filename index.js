b359b92e037dafa6e7f5095a0ff1abbf9c3ff2e4
Marc Malliate <marc.malliate@gmail.com>
2021-11-12 07:43:00 +1030

    Classes built and added to lib folder.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+// lib.js files required
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
